62257e3804a9eab8f0db68b6773c81da84e393c0
Adriano Amaral <adriano.amaral147@gmail.com>
2020-10-19 11:37:48 -0300

    DecisionTree

--- Topicos/Mini-projeto/DecisionTree.py
+++ Topicos/Mini-projeto/DecisionTree.py
@@ -12,11 +12,11 @@ import pandas as pd
 
 
 # url = "https://raw.githubusercontent.com/adrianonna/p6/master/Topicos/Mini-projeto/Segmentation-adjusted.data"
-# url = "Segmentation-adjusted.data"
+url = "Segmentation-adjusted.data"
 
 #Segmentation
-# col_names = ['label', 'x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7', 'x8', 'x9', 'x10', 'x11', 'x12', 'x13', 'x14', 'x15', 'x16', 'x17', 'x18', 'x19']
-# feature_cols = ['x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7', 'x8', 'x9', 'x10', 'x11', 'x12', 'x13', 'x14', 'x15', 'x16', 'x17', 'x18', 'x19']
+col_names = ['label', 'x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7', 'x8', 'x9', 'x10', 'x11', 'x12', 'x13', 'x14', 'x15', 'x16', 'x17', 'x18', 'x19']
+feature_cols = ['x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7', 'x8', 'x9', 'x10', 'x11', 'x12', 'x13', 'x14', 'x15', 'x16', 'x17', 'x18', 'x19']
 
 
 #PARKINSONS
@@ -37,17 +37,26 @@ y = dataset.label # Saída
 
 # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=None, stratify=y) # 80% treino e 20% teste
 
-#Letter
-X_train = pd.concat([X[0:6000],X[7000:12000],X[13000:18000]])
-y_train = pd.concat([y[0:6000],y[7000:12000],y[13000:18000]])
 
-X_test = pd.concat([X[6000:7000],X[12000:13000],X[18000:20000]])
-y_test = pd.concat([y[6000:7000],y[12000:13000],y[18000:20000]])
+#WaveForm(v2)
+# X_train = pd.concat([X[0:1666],X[2000:3666],X[4000:4666]])
+# y_train = pd.concat([y[0:1666],y[2000:3666],y[4000:4666]])
+#
+# X_test = pd.concat([X[1666:2000],X[3666:4000],X[4666:5000]])
+# y_test = pd.concat([y[1666:2000],y[3666:4000],y[4666:5000]])
+
+
+#Segmentation
+X_train = pd.concat([X[0:560],X[700:1260],X[1400:1960]])
+y_train = pd.concat([y[0:560],y[700:1260],y[1400:1960]])
+
+X_test = pd.concat([X[560:700],X[1260:1400],X[1960:2100]])
+y_test = pd.concat([y[560:700],y[1260:1400],y[1960:2100]])
 
 print(y_test)
 
 # Treinamendo da Árvore de Decisão (aprendizado Eager)
-model = tree.DecisionTreeClassifier(criterion="entropy")
+model = tree.DecisionTreeClassifier(criterion="gini")
 model = model.fit(X_train, y_train)
 
 # Predição e Resultados
